970c4256e4abfd5126564f114ba5b8b93d9ed9c9
so-brl <sonia.brunel@le-campus-numerique.fr>
2020-02-14 09:42:23 +0100

    Instanciationde la classe Fish

--- app.php
+++ app.php
@@ -1,3 +1,20 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 //--------------------------------------------------
+
+$poisson1 = new \App\Animals\Fish('Giselle');
+$poisson1-> getName();
+$poisson1-> type();
+$poisson1-> noise();
+
+echo '<br>';
+
+
+function creatAnimals($type, $nbr){
+    $animal
+
+    new \App\Animals\$type('$name');
+
+
+
+}
